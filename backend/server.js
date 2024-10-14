cce78a8eb4f08c401ec8e7f9ba71633b5acf2cbb
srhqmp <ballanosarah@gmail.com>
2024-10-14 10:34:43 +0800

    feat: setup error handler

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 import dotenv from "dotenv";
 import express from "express";
+
+// routes
 import userRoutes from "./routes/userRoutes.js";
+
+// middlewares
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config({ path: ".env.local" });
 
@@ -13,5 +18,8 @@ const app = express();
 app.get("/", (req, res) => res.send("API running"));
 
 app.use("/api/users", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
