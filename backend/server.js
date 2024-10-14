c753bcb77ea131b91121264a9c2160bf52a7054d
srhqmp <ballanosarah@gmail.com>
2024-10-14 11:01:42 +0800

    feat: create User model

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+import connectDB from "./config/db.js";
 
 // routes
 import userRoutes from "./routes/userRoutes.js";
@@ -9,9 +10,8 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config({ path: ".env.local" });
 
-console.log(process.env.PORT);
-
 const PORT = process.env.PORT || 5000;
+connectDB();
 
 const app = express();
 
